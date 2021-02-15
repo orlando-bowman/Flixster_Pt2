1a572aa54b174e0fcb0988e667565dbd4a03d754
orlando-bowman <orlandobowman76@gmail.com>
2021-02-14 20:46:30 -0500

    commit

--- app/src/main/java/com/example/flixster/Models/Movies.java
+++ app/src/main/java/com/example/flixster/Models/Movies.java
@@ -3,22 +3,30 @@ package com.example.flixster.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movies {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    public Movies() {}
 
     public Movies(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movies> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -41,6 +49,14 @@ public class Movies {
     }
 
     public String getOverview() {
-        return overview;
+            return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
